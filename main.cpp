9aabd0f238331a915a0e1f4288cbd79c9e86c64e
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-04-11 19:48:08 -0400

    cleaned main and task A tests

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main(){
   std::cout << "the user name of the first user is: " << p1.getUsername() << std::endl; // marco
   std::cout << "the full name of the first user is: " << p1.getFullName() << std::endl; // Marco (@marco)
 
-  p1.setDisplayName("Marco Rossi"); 
-  std::cout << p1.getUsername() << std::endl; // marco
-  std::cout << p1.getFullName() << std::endl; // Marco Rossi (@marco)
+  p1.setDisplayName("Marco Rossi");
+  std::cout << "\nthe first user made a change \n\nthe user name of the first user is still " << p1.getUsername() << std::endl; // marco
+  std::cout << "the full name of the first user is now " << p1.getFullName() << std::endl; // Marco Rossi (@marco)
     
   Profile p2("tarma1", "Tarma Roving");    
-  std::cout << p2.getUsername() << std::endl; // tarma1
-  std::cout << p2.getFullName() << std::endl; // Tarma Roving (@tarma1)
+  std::cout << "\nthe user name of the second user is: " << p2.getUsername() << std::endl; // tarma1
+  std::cout << "the full name of the second user is: " << p2.getFullName() << std::endl; // Tarma Roving (@tarma1)
   
   return 0;
 }
---
+++ tests.cpp
@@ -0,0 +1,9 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "profile.h"
+
+TEST_CASE("username display"){
+  Profile p1("tiffanyzhu", "Tiffany Zhu");
+  CHECK(p1.getUsername() == "tiffanyzhu");
+  CHECK(p1.getFullName() == "Tiffany Zhu (@tiffanyzhu)");
+}
