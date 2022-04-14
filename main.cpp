2038dd15e494ff7b4da1095694c6fda6ee2b5197
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-04-13 21:19:08 -0400

    organized couts

--- main.cpp
+++ main.cpp
@@ -22,18 +22,19 @@ int main(){
   std::cout << "\n--------------Task B------------------" << std::endl;
   Network nw;
 
-  std::cout << nw.addUser("mario", "Mario") << std::endl;     // true (1)
-  std::cout << nw.addUser("luigi", "Luigi") << std::endl;     // true (1)
+  std::cout << "Accounts which are added to the network -> 1" << std::endl;
+  std::cout << "mario: " << nw.addUser("mario", "Mario") << std::endl;     // true (1)
+  std::cout << "luigi: " << nw.addUser("luigi", "Luigi") << std::endl;     // true (1)
 
-  std::cout << nw.addUser("mario", "Mario2") << std::endl;    // false (0)
-  std::cout << nw.addUser("mario 2", "Mario2") << std::endl;  // false (0)
-  std::cout << nw.addUser("mario-2", "Mario2") << std::endl;  // false (0)
+  std::cout << "mario: " << nw.addUser("mario", "Mario2") << std::endl;    // false (0)
+  std::cout << "mario 2: " << nw.addUser("mario 2", "Mario2") << std::endl;  // false (0)
+  std::cout << "mario-2: " << nw.addUser("mario-2", "Mario2") << std::endl;  // false (0)
 
   for(int i = 2; i < 20; i++){
-     std::cout << nw.addUser("mario" + std::to_string(i), "Mario" + std::to_string(i)) << std::endl;   // true (1)
+    std::cout << "mario " << std::to_string(i) << ": " << nw.addUser("mario" + std::to_string(i), "Mario" + std::to_string(i)) << std::endl;   // true (1)
   }
 
-   std::cout << nw.addUser("yoshi", "Yoshi") << std::endl;     // false (0)
+  std::cout << "yoshi: " << nw.addUser("yoshi", "Yoshi") << std::endl;     // false (0)
 
   return 0;
 }
