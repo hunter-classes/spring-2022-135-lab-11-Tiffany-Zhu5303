f55293d04f7bfbcfbb66ac18587ccdf2adfd7e9f
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-04-13 21:03:19 -0400

    Task B tests

--- tests.cpp
+++ tests.cpp
@@ -8,3 +8,18 @@ TEST_CASE("username display"){
   CHECK(p1.getUsername() == "tiffanyzhu");
   CHECK(p1.getFullName() == "Tiffany Zhu (@tiffanyzhu)");
 }
+
+TEST_CASE("added to network"){
+  Network network;
+
+  CHECK(network.addUser("tiffanyzhuuu", "Tiffany Zhu") == 1);
+  CHECK(network.addUser("username", "Elton Chan") == 1);
+  CHECK(network.addUser("r1sus@na", "Elton Chan") == 0);
+  CHECK(network.addUser(" ", "Blank") == 0);
+  
+  for(int i = 2; i < 20; i++){
+    network.addUser("tiffany" + std::to_string(i), "Tiffany" + std::to_string(i));
+  }
+
+  CHECK(network.addUser("halloooo", "random") == 0);
+}
